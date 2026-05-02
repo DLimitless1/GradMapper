3fd50ac6cafda6b320c856aecd3921baa2dce087
Davina Basham <74201041+DLimitless1@users.noreply.github.com>
2026-05-02 15:55:23 -0700

    Delete grad-mapper directory
    Remove nested grad-mapper folder

--- grad-mapper/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/grad-mapper/',   // Important for GitHub Pages if repo name is grad-mapper
-});
